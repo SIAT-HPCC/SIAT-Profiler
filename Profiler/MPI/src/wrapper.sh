e781d2c105ea907ed6e3e2cc511bbb919239ae2e
zhuangbility111 <549694389@qq.com>
2022-08-14 22:35:17 +0800

    update wrapper.sh

--- Profiler/MPI/src/wrapper.sh
+++ Profiler/MPI/src/wrapper.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#
+/bin/bash
 
 #if [ $(($OMPI_COMM_WORLD_RANK%3)) == 0 ] ; then
 #    cp ../counter.txt /tmp/counter${OMPI_COMM_WORLD_RANK}.txt
@@ -9,8 +10,5 @@
 #fi
 
 cp ../counter.txt /tmp/counter${OMPI_COMM_WORLD_RANK}.txt
-rocprof -i /tmp/counter${OMPI_COMM_WORLD_RANK}.txt ../cluster ../testData.fasta
+rocprof -i /tmp/counter${OMPI_COMM_WORLD_RANK}.txt {running command} 
 rm -rf  /tmp/counter${OMPI_COMM_WORLD_RANK}.txt
-#!/bin/bash
-export LD_PRELOAD=./pmpi.so
-mpirun -n 2 ./a.out
